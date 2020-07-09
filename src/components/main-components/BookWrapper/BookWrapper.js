e3de067c14f2029045bbcb7abb39edafb921cadd
IvanNaida <ivannaida8@gmail.com>
2020-07-09 16:31:01 +0300

    add size change

--- src/components/main-components/BookWrapper/BookWrapper.js
+++ src/components/main-components/BookWrapper/BookWrapper.js
@@ -6,6 +6,51 @@ import { fromEvent } from 'rxjs';
 import { skipUntil, takeUntil } from 'rxjs/operators';
 
 export default class BookWrapper extends React.Component {
+  constructor(){
+    super()
+    this.state  = {
+      sideOneStyle: {
+        height: '300px',
+        width: '300px',
+        transform: 'translate3d(150px, 0px, 150px)',
+        backgroundColor: 'rgb(128, 128, 128)',
+      },
+      sideTwoStyle: {
+        height: '300px',
+        width: '300px',
+        backgroundColor: 'rgb(181, 43, 43)',
+        transform: 'rotateY(90deg) rotateX(90deg) translate3d(0px, 150px, 150px)'
+      },
+      sideThreeStyle: {
+        height: '300px',
+        width: '300px',
+        transform: 'rotateY(90deg) rotateX(90deg) translate3d(0px, 150px, -150px)',
+        backgroundColor: 'rgb(140, 216, 53)'
+      },
+      sideFourStyle: {
+        bottom: '0',
+        height: '300px',
+        width: '300px',
+        backgroundColor: 'green',
+        transform: 'rotateY(90deg) translate3d(0px, 0px, 300px)'
+      },
+      sideFiveStyle: {
+        height: '300px',
+        width: '300px',
+        backgroundColor: 'rgb(128, 128, 128)',
+        transform: 'rotateY(-90deg) translate3d(0px, 0px, 0px)'
+      },
+      sideSixStyle: {
+        height: '300px',
+        width: '300px',
+        backgroundColor: 'rgb(128, 128, 128)',
+        transform: 'rotateY(180deg) translate3d(-150px, 0px, 150px)'
+      },
+
+    }
+  }
+
+
     rotateAngle = {
     transform:'',
     height: '',
@@ -19,27 +64,31 @@ export default class BookWrapper extends React.Component {
     mousedown$;
     mouseup$;
 
-    sideOneStyle;
-    sideTwoStyle;
-    sideThreeStyle;
-    sideFourStyle;
-    sideFiveStyle;
-    sideSixStyle;
+    // sideOneStyle;
+    // sideTwoStyle;
+    // sideThreeStyle;
+    // sideFourStyle;
+    // sideFiveStyle;
+    // sideSixStyle;
 
     componentDidMount(){
         let top;
         let config = { attributes: true };
         SizeManager.OneStyle.subscribe(e=>{
-            this.sideOneStyle = SizeManager.setStyleSideOne(e)
-            this.sideTwoStyle  = SizeManager.setStyleSideTwo(e)
-            this.sideFourStyle = SizeManager.setStyleSideFour(e)
-            this.sideFiveStyle = SizeManager.setStyleSideFive(e)
-            this.sideSixStyle = SizeManager.setStyleSideSix(e)
+            this.setState({
+              sideOneStyle:SizeManager.setStyleSideOne(e),
+              sideTwoStyle: SizeManager.setStyleSideTwo(e),
+              sideFourStyle: SizeManager.setStyleSideFour(e),
+              sideFiveStyle: SizeManager.setStyleSideFive(e),
+              sideSixStyle: SizeManager.setStyleSideSix(e),
+              sideThreeStyle: SizeManager.setStyleSideThree(e)
+            })
             new MutationObserver(mutations => {
                 console.log(mutations)
                 top = mutations[0].target
                 top = top.offsetTop
-                this.sideThreeStyle = SizeManager.setStyleSideThree(e, top)
+                this.setState({
+                })
             }).observe(document.getElementById('side3'), config);
         })
 
@@ -56,15 +105,15 @@ export default class BookWrapper extends React.Component {
     }
     render() {
         return (
-            <div className="book_wrapper" onClick={()=>{console.log(this.sideOneStyle)}}>
+            <div className="book_wrapper">
                 <div id="wrapper">
                     <div id="book">
-                        <div className="side" id="side1">1</div>
-                        <div className="side" id="side2">2</div>
-                        <div className="side" id="side3">3</div>
-                        <div className="side" id="side4">4</div>
-                        <div className="side" id="side5">5</div>
-                        <div className="side" id="side6">6</div>
+                        <div className="side" id="side1" style={this.state.sideOneStyle}>1</div>
+                        <div className="side" id="side2" style={this.state.sideTwoStyle}>2</div>
+                        <div className="side" id="side3" style={this.state.sideThreeStyle}>3</div>
+                        <div className="side" id="side4" style={this.state.sideFourStyle}>4</div>
+                        <div className="side" id="side5" style={this.state.sideFiveStyle}>5</div>
+                        <div className="side" id="side6" style={this.state.sideSixStyle}>6</div>
                     </div>
                 </div>
             </div>
@@ -84,7 +133,7 @@ export default class BookWrapper extends React.Component {
         mousemove$ = mousemove$.pipe(skipUntil(this.mousedown$));
         mousemove$ = mousemove$.pipe(takeUntil(this.mouseup$));
         this.sub = mousemove$.subscribe((e) => {
-          this.rotateAngle.transform = `rotateX(-${this.positionY+ (e.clientY - this.mouseY)}deg) rotateY(${this.positionX+ (e.clientX - this.mouseX)}deg)`
+          this.rotateAngle.transform = `rotateX(-${this.positionY+ ((e.clientY - this.mouseY)*0.7)}deg) rotateY(${this.positionX+ ((e.clientX - this.mouseX)*0.7)}deg)`
           document.getElementById("book").style.transform = this.rotateAngle.transform
         })
       }
